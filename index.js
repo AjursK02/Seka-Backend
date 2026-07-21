de427d581a98e235128ed793c7571f9c44233598
chinmaya <moharanachinmay300@gmail.com>
2026-07-21 18:15:31 +0530

    update

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const defaultOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://seka-frontend-black.vercel.app",
+  "https://seka.ajursinsights.com",
 ];
 
 const allowedOrigins = new Set(
